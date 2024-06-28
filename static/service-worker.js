fb39d61b8f5f5853ec8e7195ea7b4075b33204a1
JPLeVangie <joepine221@gmail.com>
2024-06-28 15:20:09 -0400

    adjust icons and PWA ability

--- static/service-worker.js
+++ static/service-worker.js
@@ -5,8 +5,26 @@ self.addEventListener('install', event => {
                 '/',
                 '/static/style.css',
                 '/static/manifest.json',
+                '/static/icons/lazydjicon-192x192.png',
+                '/static/icons/lazydjicon-512x512.png'
                 // Add other files you want to cache
             ]);
+        })
+    );
+    self.skipWaiting(); // Activate the new service worker immediately
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = ['lazydj-v1'];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
@@ -14,7 +32,12 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+            return response || fetch(event.request).then(fetchResponse => {
+                return caches.open('lazydj-v1').then(cache => {
+                    cache.put(event.request, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            });
+        }).catch(() => caches.match('/fallback.html'))
     );
 });
